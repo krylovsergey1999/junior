01541e2c56c0ddc885afec4d3745cd1937f27662
Krylov Sergey <kssergeyru@gmail.com>
2019-03-09 18:50:36 +0300

    Исправление замечания от ментора.

--- chapter_001/src/main/java/iterator/two/EvenNumbersIterator.java
+++ chapter_001/src/main/java/iterator/two/EvenNumbersIterator.java
@@ -2,21 +2,41 @@ package iterator.two;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterable<Integer> {
-    private ArrayList<Integer> list = new ArrayList<>();
+    private int[] arr;
+    private int size;
+    private int position = 0;
 
     @Override
     public Iterator<Integer> iterator() {
-        Iterator<Integer> it = list.iterator();
+        Iterator<Integer> it = new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                while (position < size) {
+                    if (arr[position] % 2 == 0) {
+                        return true;
+                    } else {
+                        position++;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                if (hasNext()) {
+                    return arr[position++];
+                }
+                throw new NoSuchElementException();
+            }
+        };
         return it;
     }
 
     public EvenNumbersIterator(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                this.list.add(array[i]);
-            }
-        }
+        this.arr = array;
+        this.size = array.length;
     }
 }
