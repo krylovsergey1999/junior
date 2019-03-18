c209d6aa06b427bd39fd8785d53b6215d3daf81f
Krylov Sergey <kssergeyru@gmail.com>
2019-03-18 11:47:35 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#113071]

--- chapter_001/src/main/java/list/six/Node.java
+++ chapter_001/src/main/java/list/six/Node.java
@@ -4,6 +4,10 @@ public class Node<T> {
     T value;
     Node<T> next;
 
+    public Node(T value) {
+        this.value = value;
+    }
+
     public static boolean hasCycle(Node first) {
         int maxStep = 100;
         int step = 0;
---
+++ chapter_001/src/test/java/list/six/NodeTest.java
@@ -0,0 +1,41 @@
+package list.six;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class NodeTest {
+    Node first;
+    Node two;
+    Node third;
+    Node four;
+
+    @Before
+    public void beforeTest() {
+        first = new Node(1);
+        two = new Node(2);
+        third = new Node(3);
+        four = new Node(4);
+    }
+
+    @Test
+    public void whenHasNotCycle() {
+        first.next = two;
+        two.next = third;
+        third.next = four;
+        four.next = first;
+        assertFalse(Node.hasCycle(first));
+    }
+
+    @Test
+    public void whenHasCycle() {
+        first.next = two;
+        two.next = third;
+        third.next = two;
+        four.next = first;
+        assertTrue(Node.hasCycle(first));
+    }
+}
