cc2835311f631a05e799aaa714ddd9a611578ce8
Krylov Sergey <kssergeyru@gmail.com>
2019-03-20 14:59:34 +0300

    2. Удаление запрещенных слов[#113106] Добавил stream

--- chapter_002/src/main/java/input/drop/Drop.java
+++ chapter_002/src/main/java/input/drop/Drop.java
@@ -1,20 +1,19 @@
 package input.drop;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Drop {
     void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
         try (BufferedReader input = new BufferedReader(new InputStreamReader(in));
              PrintWriter output = new PrintWriter(out)) {
-            String line;
-            while ((line = input.readLine()) != null) {
-                for (String el : abuse) {
-                    if (line.contains(el)) {
-                        line = line.replace(el, "");
-                    }
-                }
-                output.write(line);
+            final String[] line = new String[1];
+            while ((line[0] = input.readLine()) != null) {
+                Stream<String> stream = Arrays.stream(abuse);
+                stream.forEach(el -> line[0] = line[0].replace(el, ""));
+                output.write(line[0]);
             }
         }
     }
