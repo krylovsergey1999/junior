ac65d69120e9c09081a354320b60a0aabf6a923c
Krylov Sergey <kssergeyru@gmail.com>
2019-06-12 13:51:34 +0300

    Zip error

--- chapter_002/src/main/java/input/archive/Args.java
+++ chapter_002/src/main/java/input/archive/Args.java
@@ -1,15 +1,25 @@
 package input.archive;
 
 public class Args {
-    public void directory() {
+    public static String directory(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-d")) {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
+
+    public static void excule() {
 
     }
 
-    public void excule() {
-
-    }
-
-    public void output() {
-
+    public static String output(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-o")) {
+                return args[i + 1];
+            }
+        }
+        return null;
     }
 }
